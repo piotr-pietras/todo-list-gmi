fddbc3cd65c48f658a8757c5a2afe6d55c0d1778
Piotr Pietras <Piotr.Pietras@lyondellbasell.com>
2024-06-28 18:18:12 +0200

    handle prisma exceptions

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Task } from './interface/task.interface';
 import { TaskAddDto } from './dto/task-add.dto';
@@ -13,27 +13,66 @@ export class TaskService {
   }
 
   async getById(id: string): Promise<Task> {
-    return this.prisma.task.findUnique({ where: { id: parseInt(id) } });
+    try {
+      const res = await this.prisma.task.findUnique({
+        where: { id: parseInt(id) },
+      });
+      if (!res) {
+        throw new BadRequestException('Bad request', {
+          description: 'Record does not exist.',
+        });
+      }
+      return res;
+    } catch (error) {
+      throw new BadRequestException('Bad request', {
+        cause: error,
+        description: error.meta.cause,
+      });
+    }
   }
 
   async addOne(taskAddDto: TaskAddDto): Promise<Task> {
-    return this.prisma.task.create({
-      data: {
-        ...taskAddDto,
-      },
-    });
+    try {
+      const res = await this.prisma.task.create({
+        data: {
+          ...taskAddDto,
+        },
+      });
+      return res;
+    } catch (error) {
+      throw new BadRequestException('Bad request', {
+        cause: error,
+        description: error.meta.cause,
+      });
+    }
   }
 
   async updateOne(id: string, taskUpdateDto: TaskUpdateDto): Promise<Task> {
-    return this.prisma.task.update({
-      where: { id: parseInt(id) },
-      data: { ...taskUpdateDto },
-    });
+    try {
+      const result = await this.prisma.task.update({
+        where: { id: parseInt(id) },
+        data: { ...taskUpdateDto },
+      });
+      return result;
+    } catch (error) {
+      throw new BadRequestException('Bad request', {
+        cause: error,
+        description: error.meta.cause,
+      });
+    }
   }
 
   async deleteOne(id: string): Promise<Task> {
-    return this.prisma.task.delete({
-      where: { id: parseInt(id) },
-    });
+    try {
+      const result = await this.prisma.task.delete({
+        where: { id: parseInt(id) },
+      });
+      return result;
+    } catch (error) {
+      throw new BadRequestException('Bad request', {
+        cause: error,
+        description: error.meta.cause,
+      });
+    }
   }
 }
